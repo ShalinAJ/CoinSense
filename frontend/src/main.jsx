0234ed951eae7834587b725b2787cf91ebdead73
Shalin Jayatilleke <77483574+ShalinAJ@users.noreply.github.com>
2024-04-13 14:56:31 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-=======
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -18,5 +8,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthContextProvider>
     <App />
   </AuthContextProvider>
->>>>>>> fa1b7f214137a1c7745daed78729519ab950a829
 );
