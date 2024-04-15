9eaf0634f5705b41ca6e15006459c719784588ea
ShalinAJ <andrewjayatilleke@gmail.com>
2024-04-15 19:03:12 +0530

    Transaction deletion feature

--- frontend/src/components/TransactionsTable.jsx
+++ frontend/src/components/TransactionsTable.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import AddTransactionModal from "../components/AddTransactionModal.jsx";
+import deleteImg from "../assets/delete.png";
 import { format } from "date-fns";
+import { json } from "react-router-dom";
 
 const TransactionsTable = ({ transactions }) => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -11,6 +13,24 @@ const TransactionsTable = ({ transactions }) => {
 
   const closeModal = () => {
     setModalOpen(false);
+  };
+
+  const deleteHandler = async (id) => {
+    console.log(id);
+    const user = JSON.parse(localStorage.getItem("user"));
+    const response = await fetch("http://localhost:4000/" + id, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Could not delete transaction.");
+    }
+
+    window.location.reload();
+    return null;
   };
 
   return (
@@ -35,10 +55,10 @@ const TransactionsTable = ({ transactions }) => {
           <table className="w-[100%]">
             <tbody>
               <tr className="text-left text-sm leading-[45px]">
-                <th>transactions</th>
+                <th>Transactions</th>
                 <th>Date</th>
                 <th>Amount</th>
-                <th className="text-center">Status</th>
+                <th className="text-center pl-[80px]">Status</th>
               </tr>
               {transactions.map((transaction) => (
                 <tr
@@ -60,19 +80,25 @@ const TransactionsTable = ({ transactions }) => {
                       currency: "USD",
                     }).format(transaction.amount)}
                   </td>
-                  <td className="mt-3 text-center text-xs flex justify-center">
+                  <td className="mt-3 text-center text-xs flex justify-end pr-9">
                     <p
                       className={
                         (transaction.status === "Income" &&
-                          "py-1 w-[50%] bg-[#bcffde] text-[#02B15A] rounded-xl") ||
+                          "py-1 w-[45%] bg-[#bcffde] text-[#02B15A] rounded-xl") ||
                         (transaction.status === "Expense" &&
-                          "py-1 w-[50%] bg-[#ff00001f] text-[#ff0000] rounded-xl") ||
+                          "py-1 w-[45%] bg-[#ff00001f] text-[#ff0000] rounded-xl") ||
                         (transaction.status === "Investment" &&
-                          "py-1 w-[50%] bg-[#bcffde] text-[#02B15A] rounded-xl")
+                          "py-1 w-[45%] bg-[#bcffde] text-[#02B15A] rounded-xl")
                       }
                     >
                       {transaction.status}
                     </p>
+                    <button
+                      onClick={() => deleteHandler(transaction._id)}
+                      className="p-0 m-0 ml-2 border-none bg-transparent"
+                    >
+                      <img src={deleteImg} className="w-4" />
+                    </button>
                   </td>
                 </tr>
               ))}
