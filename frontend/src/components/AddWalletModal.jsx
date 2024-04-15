f0be91c04819a990ab8530b314f6b60236cc3a7e
ShalinAJ <andrewjayatilleke@gmail.com>
2024-04-15 23:30:25 +0530

    Add wallet modal

---
+++ frontend/src/components/AddWalletModal.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { Form } from "react-router-dom";
+
+const AddWalletModal = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  const closeModal = () => {
+    onClose();
+  };
+
+  return (
+    <>
+      <div
+        className="fixed inset-0 bg-black opacity-50 z-50"
+        onClick={closeModal}
+      ></div>
+      <dialog open className="mt-[90px] w-[40%] z-50 rounded-xl">
+        <div className="z-4 px-7 pb-9 flex flex-row rounded-xl">
+          <div className="w-[100%] leading-6">
+            <div className="flex flex-wrap justify-between items-center pt-2">
+              <h2 className="font-semibold">Add Wallet</h2>
+              <button
+                className="bg-white border-none pr-0 text-black"
+                onClick={closeModal}
+              >
+                &#10006;
+              </button>
+            </div>
+
+            <Form method="post" className="flex flex-col">
+              <div className="flex flex-col mb-2 mt-8">
+                <label htmlFor="">Name on card</label>
+                <input id="name" type="text" name="name"></input>
+              </div>
+              <div className="flex flex-col my-3">
+                <label htmlFor="">Card number</label>
+                <input
+                  id="cardnumber"
+                  type="text"
+                  name="cardnumber"
+                  maxlength="16"
+                ></input>
+              </div>
+              <div className="flex flex-row items-center my-4 ">
+                <label htmlFor="">Expiry date : </label>
+                <input
+                  id="month"
+                  type="text"
+                  name="month"
+                  maxLength="2"
+                  className="w-[10%] mx-2"
+                ></input>
+                /
+                <input
+                  id="year"
+                  type="text"
+                  name="year"
+                  maxLength="2"
+                  className="w-[10%] mx-2"
+                ></input>
+              </div>
+              {/* not sure about the bottom input field */}
+              {/* 
+              <div className="flex flex-row items-center my-3">
+                <label htmlFor="">Last balance :</label>
+                <input
+                  id="lastbalance"
+                  type="float"
+                  name="lastbalance"
+                  className="w-[40%] ml-3 "
+                ></input>
+              </div> */}
+              <div className="flex justify-end">
+                <button className="mt-8 py-1 px-3 w-[25%] text-sm items-end hover:bg-coinsense-blue-darker">
+                  Add
+                </button>
+              </div>
+            </Form>
+          </div>
+        </div>
+      </dialog>
+    </>
+  );
+};
+
+export default AddWalletModal;
--- frontend/src/pages/Wallet.jsx
+++ frontend/src/pages/Wallet.jsx
@@ -1,21 +1,40 @@
+import { useState } from "react";
+import AddWalletModal from "../components/AddWalletModal";
+
 const WalletPage = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <div className="w-[80%] ">
-      <div className="flex items-start justify-between px-[28px] pt-[45px]">
-        <div>
-          <h2 className="text-2xl font-bold">Wallets</h2>
-          <p className="text-sm pt-2 font-light">
-            Detailed view of your walletss
-          </p>
+    <>
+      <AddWalletModal isOpen={modalOpen} onClose={closeModal} />
+      <div className="w-[80%] ">
+        <div className="flex items-start justify-between px-[28px] pt-[45px]">
+          <div>
+            <h2 className="text-2xl font-bold">Wallets</h2>
+            <p className="text-sm pt-2 font-light">
+              Detailed view of your walletss
+            </p>
+          </div>
+          <button
+            onClick={openModal}
+            className="bg-[#152DFF] text-white text-xs px-10 hover:bg-coinsense-blue-darker"
+          >
+            Add A New Wallet
+          </button>
         </div>
-        <button className="bg-[#152DFF] text-white text-xs px-10 hover:bg-coinsense-blue-darker">
-          Add A New Wallet
-        </button>
+        <div className=" pl-[38px] py-[36px]">
+          <div>add cards..</div>
+        </div>
       </div>
-      <div className=" pl-[38px] py-[36px]">
-        <div>add cards..</div>
-      </div>
-    </div>
+    </>
   );
 };
 
