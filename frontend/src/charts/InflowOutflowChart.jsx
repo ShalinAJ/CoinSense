007e5b793493d7861ea924368a768c84a521871d
ShalinAJ <andrewjayatilleke@gmail.com>
2024-04-17 14:57:04 +0530

    Adding transaction data to charts

--- frontend/src/charts/InflowOutflowChart.jsx
+++ frontend/src/charts/InflowOutflowChart.jsx
@@ -1,10 +1,219 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Chart from "chart.js/auto";
 
 const InflowOutflowChart = () => {
-  useEffect(() => {
-    const ctx = document.getElementById("inflowOutflow");
+  const inflows = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const outflows = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+  async function loadTransactions() {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const response = await fetch("http://localhost:4000/transactions", {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+
+    if (!response.ok) {
+      return json(
+        { message: "Could not fetch transactions." },
+        { status: 500 }
+      );
+    } else {
+      const transactions = await response.json();
+      return transactions;
+    }
+  }
+
+  useEffect(() => {
+    async function transactionInfo() {
+      try {
+        const transactions = await loadTransactions();
+
+        let inflowCounter = 0;
+        let outflowCounter = 0;
+
+        transactions.forEach((transaction) => {
+          const transactionDate = new Date(transaction.date).toLocaleString(
+            "en-US",
+            {
+              month: "long",
+            }
+          );
+
+          if (
+            transactionDate === "January" &&
+            transaction.status === "Income"
+          ) {
+            inflowCounter = transaction.amount;
+            inflows[0] = inflowCounter;
+            return inflows;
+          }
+
+          if (
+            transactionDate === "February" &&
+            transaction.status === "Income"
+          ) {
+            inflowCounter = transaction.amount;
+            inflows[1] = inflowCounter;
+            return inflows;
+          }
+          if (transactionDate === "March" && transaction.status === "Income") {
+            inflowCounter = transaction.amount;
+            inflows[2] = inflowCounter;
+            return inflows;
+          }
+          if (transactionDate === "April" && transaction.status === "Income") {
+            inflowCounter = transaction.amount;
+            inflows[3] = inflowCounter;
+            return inflows;
+          }
+          if (transactionDate === "May" && transaction.status === "Income") {
+            inflowCounter = transaction.amount;
+            inflows[4] = inflowCounter;
+            return inflows;
+          }
+          if (transactionDate === "June" && transaction.status === "Income") {
+            inflowCounter = transaction.amount;
+            inflows[5] = inflowCounter;
+            return inflows;
+          }
+          if (transactionDate === "Jully" && transaction.status === "Income") {
+            inflowCounter = transaction.amount;
+            inflows[6] = inflowCounter;
+            return inflows;
+          }
+          if (transactionDate === "Augest" && transaction.status === "Income") {
+            inflowCounter = transaction.amount;
+            inflows[7] = inflowCounter;
+            return inflows;
+          }
+          if (
+            transactionDate === "September" &&
+            transaction.status === "Income"
+          ) {
+            inflowCounter = transaction.amount;
+            inflows[8] = inflowCounter;
+            return inflows;
+          }
+          if (
+            transactionDate === "October" &&
+            transaction.status === "Income"
+          ) {
+            inflowCounter = transaction.amount;
+            inflows[9] = inflowCounter;
+            return inflows;
+          }
+          if (
+            transactionDate === "November" &&
+            transaction.status === "Income"
+          ) {
+            inflowCounter = transaction.amount;
+            inflows[10] = inflowCounter;
+            return inflows;
+          }
+          if (
+            transactionDate === "December" &&
+            transaction.status === "Income"
+          ) {
+            inflowCounter = transaction.amount;
+            inflows[11] = inflowCounter;
+            return inflows;
+          }
+
+          if (
+            transactionDate === "January" &&
+            transaction.status === "Expense"
+          ) {
+            outflowCounter = transaction.amount;
+            outflows[0] = outflowCounter;
+            return outflows;
+          }
+
+          if (
+            transactionDate === "February" &&
+            transaction.status === "Expense"
+          ) {
+            outflowCounter = transaction.amount;
+            outflows[1] = outflowCounter;
+            return outflows;
+          }
+          if (transactionDate === "March" && transaction.status === "Expense") {
+            outflowCounter = transaction.amount;
+            outflows[2] = outflowCounter;
+            return outflows;
+          }
+          if (transactionDate === "April" && transaction.status === "Expense") {
+            outflowCounter = transaction.amount;
+            outflows[3] = outflowCounter;
+            return outflows;
+          }
+          if (transactionDate === "May" && transaction.status === "Expense") {
+            outflowCounter = transaction.amount;
+            outflows[4] = outflowCounter;
+            return outflows;
+          }
+          if (transactionDate === "June" && transaction.status === "Expense") {
+            outflowCounter = transaction.amount;
+            outflows[5] = outflowCounter;
+            return outflows;
+          }
+          if (transactionDate === "Jully" && transaction.status === "Expense") {
+            outflowCounter = transaction.amount;
+            outflows[6] = outflowCounter;
+            return outflows;
+          }
+          if (
+            transactionDate === "Augest" &&
+            transaction.status === "Expense"
+          ) {
+            outflowCounter = transaction.amount;
+            outflows[7] = outflowCounter;
+            return outflows;
+          }
+          if (
+            transactionDate === "September" &&
+            transaction.status === "Expense"
+          ) {
+            outflowCounter = transaction.amount;
+            outflows[8] = outflowCounter;
+            return outflows;
+          }
+          if (
+            transactionDate === "October" &&
+            transaction.status === "Expense"
+          ) {
+            outflowCounter = transaction.amount;
+            outflows[9] = outflowCounter;
+            return outflows;
+          }
+          if (
+            transactionDate === "November" &&
+            transaction.status === "Expense"
+          ) {
+            outflowCounter = transaction.amount;
+            outflows[10] = outflowCounter;
+            return outflows;
+          }
+          if (
+            transactionDate === "December" &&
+            transaction.status === "Expense"
+          ) {
+            outflowCounter = transaction.amount;
+            outflows[11] = outflowCounter;
+            return outflows;
+          }
+        });
+        renderChart();
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+
+    transactionInfo();
+  }, []);
+
+  function renderChart() {
+    const ctx = document.getElementById("inflowOutflow");
     new Chart(ctx, {
       type: "bar",
       data: {
@@ -25,7 +234,7 @@ const InflowOutflowChart = () => {
         datasets: [
           {
             label: "Inflow",
-            data: [2, 3, 4, 5, 6, 12, 11, 8, 6, 4, 3, 9],
+            data: inflows,
             borderWidth: 1,
             borderColor: "#152DFF",
             backgroundColor: "#152dff26",
@@ -41,7 +250,7 @@ const InflowOutflowChart = () => {
           },
           {
             label: "Outflow",
-            data: [12, 19, 3, 5, 2, 3, 18, 12, 7, 3, 5, 9],
+            data: outflows,
             borderWidth: 1,
             borderColor: "#c4c4c4",
             backgroundColor: "#f3f3f3e8",
@@ -95,7 +304,7 @@ const InflowOutflowChart = () => {
         },
       },
     });
-  }, []);
+  }
 
   return <canvas width="100" height="30" id="inflowOutflow"></canvas>;
 };
