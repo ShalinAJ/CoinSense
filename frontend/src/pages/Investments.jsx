96c6b6888d8197be4b1e4258eeda17694b343720
nadun weerakkody <weerakkody.kn@gmail.com>
2024-04-24 06:44:19 +0530

    Initial candlestick chart

--- frontend/src/pages/Investments.jsx
+++ frontend/src/pages/Investments.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import InvestmentChart from "../charts/InvestmentChart";
 
 const InvestmentsPage = () => {
   return (
     <div className="w-[80%] h-[max-content] bg-white">
       <div>Investments Page</div>
+      <div>
+        <InvestmentChart />
+      </div>
     </div>
   );
 };
