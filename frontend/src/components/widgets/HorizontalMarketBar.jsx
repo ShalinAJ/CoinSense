116ba5de195542e48d5b600f9f0d38a01d109d17
nadun weerakkody <weerakkody.kn@gmail.com>
2024-04-26 21:00:12 +0530

    isue

--- frontend/src/components/widgets/HorizontalMarketBar.jsx
+++ frontend/src/components/widgets/HorizontalMarketBar.jsx
@@ -23,17 +23,26 @@ const HorizontalMarketBar = () => {
     getCryptoGeneralData();
   }, []);
 
+  if (cryptoData.length === 0) {
+    // Data is still loading, return a loading indicator or null
+    return <div>Loading...</div>;
+  }
+
   //console.log(cryptoData);
   return (
     <div className="flex flex-row justify-between px-10 py-4 mb-10 rounded-3xl border shadow-sm hover:shadow-lg shadow-grey-500/40 transition-shadow duration-300">
       <div className="w-[30%] flex flex-col gap-4">
         <div className="flex flex-row items-center justify-between">
           <img
-            src={cryptoData[0].image}
+            src={cryptoData[0].image ? cryptoData[0].image : "--"}
             alt=""
             className="w-7 h-7 rounded-full"
           />
-          <p className="text-sm font-medium">{cryptoData[0].current_price}</p>
+          <p className="text-sm font-medium">
+            {cryptoData[0] && cryptoData[0].current_price
+              ? cryptoData[0].current_price
+              : "--"}
+          </p>
           <p className="text-sm font-medium text-[#02B15A]">+1.48%</p>
         </div>
         <div className="flex flex-row items-center justify-between">
