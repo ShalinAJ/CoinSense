96afe09f95fb9df58bcba3342925554336807033
nadun weerakkody <weerakkody.kn@gmail.com>
2024-05-01 11:57:11 +0530

    horizontal market bar crypto img error

--- backend/controllers/cryptoGeneralDataController.jsx
+++ backend/controllers/cryptoGeneralDataController.jsx
@@ -4,7 +4,9 @@ const CryptoGeneralData = require("../models/cryptoGeneralDataModel.jsx");
 
 const getCryptoGeneralData = async (req, res) => {
   try {
-    const data = await CryptoGeneralData.find({}).limit(3);
+    const data = await CryptoGeneralData.find({
+      market_cap_rank: { $in: [1, 2, 3] },
+    }).sort({ market_cap_rank: 1 });
     res.status(200).json(data);
   } catch (error) {
     res.status(400).json({ error: error.message });
--- frontend/src/components/widgets/HorizontalMarketBar.jsx
+++ frontend/src/components/widgets/HorizontalMarketBar.jsx
@@ -21,7 +21,6 @@ const HorizontalMarketBar = () => {
         });
 
         const cryptoDetails = await response.json();
-        console.log(cryptoDetails);
         setCryptoData(cryptoDetails);
       } catch (error) {
         console.error("Error fetching crypto data: ", error);
