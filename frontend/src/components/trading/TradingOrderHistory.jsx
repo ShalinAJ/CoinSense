b39b0b7c0d4b38bf258d46b098f1ced91cf4b4fd
ShalinAJ <andrewjayatilleke@gmail.com>
2024-05-03 20:55:53 +0530

    Order history scroll, scrollbar styling

--- frontend/src/components/trading/TradingOrderHistory.jsx
+++ frontend/src/components/trading/TradingOrderHistory.jsx
@@ -27,24 +27,34 @@ const TradingOrderHistory = () => {
   return (
     <div className="px-1 my-3">
       <hr />
-      <table className="w-[100%]">
-        <tbody>
-          <tr className="text-xs  leading-[30px]">
-            <th className="text-left font-normal">Token</th>
-            <th className="text-center font-normal">Amount</th>
-            <th className="text-right font-normal">Type</th>
-          </tr>
+      <div className="overflow-y-auto max-h-[18rem]">
+        <table className="w-full">
+          <tbody>
+            <tr className="text-xs leading-[25px]">
+              <th className="text-left font-normal">Token</th>
+              <th className="text-center font-normal">Amount</th>
+              <th className="text-right font-normal pr-3">Type</th>
+            </tr>
 
-          {orderHistory &&
-            orderHistory.map((order) => (
-              <tr className="text-xs font-semibold">
-                <td>{order.name}</td>
-                <td className="text-center">${order.amount * order.price}</td>
-                <td className="text-right">{order.transactionType}</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+            {orderHistory &&
+              orderHistory.map((order, index) => (
+                <tr
+                  key={index}
+                  className="text-xs font-semibold leading-[35px]"
+                >
+                  <td>{order.name}</td>
+                  <td className="text-center">
+                    {new Intl.NumberFormat("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    }).format(order.amount * order.price)}
+                  </td>
+                  <td className="text-right pr-3">{order.transactionType}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
