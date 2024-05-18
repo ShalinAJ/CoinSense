5a4db9ec2d66044b38deb91c2d82bb2ffb59a3b2
nadun weerakkody <weerakkody.kn@gmail.com>
2024-05-18 22:27:39 +0530

    login and register animation

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import LeftSlide from "../components/animations/LeftSlide";
+import Spring from "../components/animations/Spring";
 
 import { useLogin } from "../Hooks/useLogin";
 import addUser from "../assets/add-user.svg";
@@ -38,121 +40,131 @@ const LoginPage = () => {
     <>
       <div className="h-[100%] flex justify-between items-center bg-coinsense-blue">
         <div className="flex flex-row h-[100%] pt-[8rem]">
-          <div className="ml-[10rem] w-[80%] text-white mb-4 rounded-xl pt-10 flex flex-col">
-            <p className="text-4xl font-bold">CoinSense</p>
-            <p className="text-6xl font-bold mt-[4rem] leading-[4.5rem]">
-              Begin your journey with CoinSense
-            </p>
-            <p className="mt-10 w-[80%]">
-              Unlock exclusive features and more by logging in or registering
-              with us today. Your gateway to seamless financial management
-              starts here!
-            </p>
-          </div>
-        </div>
-        <div className=" w-[400px] px-4 py-6 border shadow-lg bg-white rounded-xl mt-20 mb-20 mr-[10rem]">
-          <form onSubmit={handleSubmit}>
-            <div className="flex text-center">
-              <img src={addUser} alt="add user sign" className="w-9 ml-20" />
-              <p className="text-black font-medium flex justify-center mt-1 ml-5">
-                Sign In
+          <LeftSlide>
+            <div className="ml-[10rem] w-[80%] text-white mb-4 rounded-xl pt-10 flex flex-col">
+              <p className="text-4xl font-bold">CoinSense</p>
+              <p className="text-6xl font-bold mt-[4rem] leading-[4.5rem]">
+                Begin your journey with CoinSense
+              </p>
+              <p className="mt-10 w-[80%]">
+                Unlock exclusive features and more by logging in or registering
+                with us today. Your gateway to seamless financial management
+                starts here!
               </p>
             </div>
-
-            <p className="mb-5 mt-6 text-[#626262] flex flex-left text-xs">
-              Sign in to your CoinSense Account
-            </p>
-
-            <div className="flex flex-col text-left mb-4">
-              <label
-                className="mb-1 text-black text-xs font-semibold"
-                htmlFor="email"
-              >
-                Email <seciton className="text-red-600">*</seciton>
-              </label>
-              <div className="relative">
-                <img
-                  src={loginInputEmail}
-                  alt=""
-                  className="absolute left-2 top-2 max-w-4"
-                />
-                <input
-                  className="rounded-md py-2 pl-8 w-80 text-xs text-black bg-white border border-grey-800 focus:outline-none focus:border-[#152DFF]"
-                  id="email"
-                  type="email"
-                  name="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <div className="text-[11px] text-red-500 pt-1 pl-1">
-                  {error && error === "Incorrect Email" && error}
-                </div>
-              </div>
-            </div>
-
-            <div className="flex flex-col text-left">
-              <label
-                className="mb-1 text-black text-xs font-semibold"
-                htmlFor="password"
-              >
-                Password <seciton className="text-red-600">*</seciton>
-              </label>
-              <div className="relative">
-                <img
-                  src={loginInputPswd}
-                  alt=""
-                  className="absolute left-2 top-2 max-w-4"
-                />
-                <input
-                  className="rounded-md py-2 pl-8 w-80 text-xs text-black bg-white border border-grey-800 focus:outline-none focus:border-[#152DFF]"
-                  id="password"
-                  type="password"
-                  name="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <div className="text-[11px] text-red-500 pt-1 pl-1">
-                  {(googleError === "" &&
-                    error === "Incorrect Password" &&
-                    error) ||
-                    (error === "All fields must be filled" && error)}
-                </div>
-              </div>
-            </div>
-
-            <div className="text-right flex justify-end my-5">
-              <a className="text-xs p-0 text-[#152DFF] font-medium" href="">
-                Forgot Password
-              </a>
-            </div>
-
-            <p>
-              <button className="w-full bg-[#152DFF] hover:bg-coinsense-blue-darker">
-                Sign in
-              </button>
-            </p>
-          </form>
-          <div className="mt-4">
-            <p className="flex content-center justify-center items-center text-xs font-medium text-black">
-              Dont have an account?
-              <Link
-                className="text-xs font-medium py-0 text-[#152DFF]"
-                to="/register"
-              >
-                Sign Up
-              </Link>
-            </p>
-          </div>
-          <div className="mt-6 mb-3 flex items-center">
-            <hr className="flex-1 border-t border-[#E0E0E0]" />
-            <span className="mx-2 text-xs text-black">OR</span>
-            <hr className="flex-1 border-t border-[#E0E0E0]" />
-          </div>
-          <div className="flex flex-col justify-center items-center pb-4">
-            <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
-            <div className="text-[11px] text-red-500 pt-1 text-center">
-              {googleError}
-            </div>
-          </div>
+          </LeftSlide>
         </div>
+        <Spring>
+          <div className=" w-[400px] px-4 py-6 border shadow-lg bg-white rounded-xl mt-20 mb-20 mr-[10rem]">
+            <Spring>
+              <form onSubmit={handleSubmit}>
+                <div className="flex text-center">
+                  <img
+                    src={addUser}
+                    alt="add user sign"
+                    className="w-9 ml-20"
+                  />
+                  <p className="text-black font-medium flex justify-center mt-1 ml-5">
+                    Sign In
+                  </p>
+                </div>
+
+                <p className="mb-5 mt-6 text-[#626262] flex flex-left text-xs">
+                  Sign in to your CoinSense Account
+                </p>
+
+                <div className="flex flex-col text-left mb-4">
+                  <label
+                    className="mb-1 text-black text-xs font-semibold"
+                    htmlFor="email"
+                  >
+                    Email <seciton className="text-red-600">*</seciton>
+                  </label>
+                  <div className="relative">
+                    <img
+                      src={loginInputEmail}
+                      alt=""
+                      className="absolute left-2 top-2 max-w-4"
+                    />
+                    <input
+                      className="rounded-md py-2 pl-8 w-80 text-xs text-black bg-white border border-grey-800 focus:outline-none focus:border-[#152DFF]"
+                      id="email"
+                      type="email"
+                      name="email"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <div className="text-[11px] text-red-500 pt-1 pl-1">
+                      {error && error === "Incorrect Email" && error}
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col text-left">
+                  <label
+                    className="mb-1 text-black text-xs font-semibold"
+                    htmlFor="password"
+                  >
+                    Password <seciton className="text-red-600">*</seciton>
+                  </label>
+                  <div className="relative">
+                    <img
+                      src={loginInputPswd}
+                      alt=""
+                      className="absolute left-2 top-2 max-w-4"
+                    />
+                    <input
+                      className="rounded-md py-2 pl-8 w-80 text-xs text-black bg-white border border-grey-800 focus:outline-none focus:border-[#152DFF]"
+                      id="password"
+                      type="password"
+                      name="password"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <div className="text-[11px] text-red-500 pt-1 pl-1">
+                      {(googleError === "" &&
+                        error === "Incorrect Password" &&
+                        error) ||
+                        (error === "All fields must be filled" && error)}
+                    </div>
+                  </div>
+                </div>
+
+                <div className="text-right flex justify-end my-5">
+                  <a className="text-xs p-0 text-[#152DFF] font-medium" href="">
+                    Forgot Password
+                  </a>
+                </div>
+
+                <p>
+                  <button className="w-full bg-[#152DFF] hover:bg-coinsense-blue-darker">
+                    Sign in
+                  </button>
+                </p>
+              </form>
+              <div className="mt-4">
+                <p className="flex content-center justify-center items-center text-xs font-medium text-black">
+                  Dont have an account?
+                  <Link
+                    className="text-xs font-medium py-0 text-[#152DFF]"
+                    to="/register"
+                  >
+                    Sign Up
+                  </Link>
+                </p>
+              </div>
+              <div className="mt-6 mb-3 flex items-center">
+                <hr className="flex-1 border-t border-[#E0E0E0]" />
+                <span className="mx-2 text-xs text-black">OR</span>
+                <hr className="flex-1 border-t border-[#E0E0E0]" />
+              </div>
+              <div className="flex flex-col justify-center items-center pb-4">
+                <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
+                <div className="text-[11px] text-red-500 pt-1 text-center">
+                  {googleError}
+                </div>
+              </div>
+            </Spring>
+          </div>
+        </Spring>
       </div>
     </>
   );
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -7,6 +7,7 @@ import loginInputPswd from "../assets/login-input-pswd.png";
 import registerInputProfile from "../assets/register-profile.png";
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
+import Spring from "../components/animations/Spring";
 
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
@@ -39,125 +40,127 @@ const RegisterPage = () => {
   return (
     <>
       <div className="h-[100%] flex flex-wrap flex-col content-center justify-center bg-coinsense-blue">
-        <div className="w-[350px] px-4 py-6 bg-white rounded-md">
-          <form onSubmit={handleSubmit}>
-            <div className="flex text-center">
-              <img src={addUser} alt="add user sign" className="w-9 ml-20" />
-              <p className="text-black font-medium flex justify-center mt-1 ml-5">
-                Sign Up
+        <Spring className="w-[350px] px-4 py-6">
+          <div className="w-[350px] px-4 py-6 bg-white rounded-md">
+            <form onSubmit={handleSubmit}>
+              <div className="flex text-center">
+                <img src={addUser} alt="add user sign" className="w-9 ml-20" />
+                <p className="text-black font-medium flex justify-center mt-1 ml-5">
+                  Sign Up
+                </p>
+              </div>
+
+              <p className="mb-5 mt-6 text-[#626262] flex flex-left text-xs">
+                Sign Up and create your CoinSense Account
+              </p>
+
+              <div className="flex flex-col text-left mb-4">
+                <label
+                  className="mb-1 text-black text-xs font-semibold"
+                  htmlFor="name"
+                >
+                  Full Name <seciton className="text-red-600">*</seciton>
+                </label>
+                <div className="relative">
+                  <img
+                    src={registerInputProfile}
+                    alt=""
+                    className="absolute left-2 top-2 max-w-4"
+                  />
+                  <input
+                    className="rounded-md py-2 pl-8 w-80 text-xs text-black bg-white border border-grey-800 focus:outline-none focus:border-[#152DFF]"
+                    id="name"
+                    type="name"
+                    name="name"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col text-left mb-4">
+                <label
+                  className="mb-1 text-black text-xs font-semibold"
+                  htmlFor="email"
+                >
+                  Email <seciton className="text-red-600">*</seciton>
+                </label>
+                <div className="relative">
+                  <img
+                    src={loginInputEmail}
+                    alt=""
+                    className="absolute left-2 top-2 max-w-4"
+                  />
+                  <input
+                    className="rounded-md py-2 pl-8 w-80 text-xs text-black bg-white border border-grey-800 focus:outline-none focus:border-[#152DFF]"
+                    id="email"
+                    type="email"
+                    name="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <div className="text-[11px] text-red-500 pt-1 pl-1">
+                    {googleError === "" &&
+                      error === "Email already in use" &&
+                      error}
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col text-left mb-6">
+                <label
+                  className="mb-1 text-black text-xs font-semibold"
+                  htmlFor="password"
+                >
+                  Password <seciton className="text-red-600">*</seciton>
+                </label>
+                <div className="relative">
+                  <img
+                    src={loginInputPswd}
+                    alt=""
+                    className="absolute left-2 top-2 max-w-4"
+                  />
+                  <input
+                    className="rounded-md py-2 pl-8 w-80 text-xs text-black bg-white border border-grey-800 focus:outline-none focus:border-[#152DFF]"
+                    id="password"
+                    type="password"
+                    name="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <div className="text-[11px] text-red-500 pt-1 pl-1">
+                    {error === "All fields must be filled" && error}
+                  </div>
+                </div>
+              </div>
+
+              <p>
+                <button className="w-full bg-[#152DFF] hover:bg-coinsense-blue-darker">
+                  Sign up
+                </button>
+              </p>
+            </form>
+            <div className="mt-4">
+              <p className="flex content-center justify-center items-center text-xs font-medium text-black">
+                Already have an account?{" "}
+                <Link
+                  className="text-xs font-medium py-0 text-[#152DFF]"
+                  to="/login"
+                >
+                  Sign In
+                </Link>
               </p>
             </div>
-
-            <p className="mb-5 mt-6 text-[#626262] flex flex-left text-xs">
-              Sign Up and create your CoinSense Account
-            </p>
-
-            <div className="flex flex-col text-left mb-4">
-              <label
-                className="mb-1 text-black text-xs font-semibold"
-                htmlFor="name"
-              >
-                Full Name <seciton className="text-red-600">*</seciton>
-              </label>
-              <div className="relative">
-                <img
-                  src={registerInputProfile}
-                  alt=""
-                  className="absolute left-2 top-2 max-w-4"
-                />
-                <input
-                  className="rounded-md py-2 pl-8 w-80 text-xs text-black bg-white border border-grey-800 focus:outline-none focus:border-[#152DFF]"
-                  id="name"
-                  type="name"
-                  name="name"
-                  onChange={(e) => setName(e.target.value)}
-                />
+            <div className="mt-6 mb-3 flex items-center">
+              <hr className="flex-1 border-t border-[#E0E0E0]" />
+              <span className="mx-2 text-xs text-black">OR</span>
+              <hr className="flex-1 border-t border-[#E0E0E0]" />
+            </div>
+            <div className="flex flex-col justify-center items-center pb-4">
+              <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
+              <div className="text-[11px] text-red-500 pt-1 text-center">
+                {googleError}
               </div>
             </div>
-
-            <div className="flex flex-col text-left mb-4">
-              <label
-                className="mb-1 text-black text-xs font-semibold"
-                htmlFor="email"
-              >
-                Email <seciton className="text-red-600">*</seciton>
-              </label>
-              <div className="relative">
-                <img
-                  src={loginInputEmail}
-                  alt=""
-                  className="absolute left-2 top-2 max-w-4"
-                />
-                <input
-                  className="rounded-md py-2 pl-8 w-80 text-xs text-black bg-white border border-grey-800 focus:outline-none focus:border-[#152DFF]"
-                  id="email"
-                  type="email"
-                  name="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <div className="text-[11px] text-red-500 pt-1 pl-1">
-                  {googleError === "" &&
-                    error === "Email already in use" &&
-                    error}
-                </div>
-              </div>
-            </div>
-
-            <div className="flex flex-col text-left mb-6">
-              <label
-                className="mb-1 text-black text-xs font-semibold"
-                htmlFor="password"
-              >
-                Password <seciton className="text-red-600">*</seciton>
-              </label>
-              <div className="relative">
-                <img
-                  src={loginInputPswd}
-                  alt=""
-                  className="absolute left-2 top-2 max-w-4"
-                />
-                <input
-                  className="rounded-md py-2 pl-8 w-80 text-xs text-black bg-white border border-grey-800 focus:outline-none focus:border-[#152DFF]"
-                  id="password"
-                  type="password"
-                  name="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <div className="text-[11px] text-red-500 pt-1 pl-1">
-                  {error === "All fields must be filled" && error}
-                </div>
-              </div>
-            </div>
-
-            <p>
-              <button className="w-full bg-[#152DFF] hover:bg-coinsense-blue-darker">
-                Sign up
-              </button>
-            </p>
-          </form>
-          <div className="mt-4">
-            <p className="flex content-center justify-center items-center text-xs font-medium text-black">
-              Already have an account?{" "}
-              <Link
-                className="text-xs font-medium py-0 text-[#152DFF]"
-                to="/login"
-              >
-                Sign In
-              </Link>
-            </p>
           </div>
-          <div className="mt-6 mb-3 flex items-center">
-            <hr className="flex-1 border-t border-[#E0E0E0]" />
-            <span className="mx-2 text-xs text-black">OR</span>
-            <hr className="flex-1 border-t border-[#E0E0E0]" />
-          </div>
-          <div className="flex flex-col justify-center items-center pb-4">
-            <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
-            <div className="text-[11px] text-red-500 pt-1 text-center">
-              {googleError}
-            </div>
-          </div>
-        </div>
+        </Spring>
       </div>
     </>
   );
