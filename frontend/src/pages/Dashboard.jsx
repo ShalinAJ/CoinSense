2659c1f9abb8d9c413ccac1afc28dfcfb5e76374
nadun weerakkody <weerakkody.kn@gmail.com>
2024-05-18 12:26:59 +0530

    animation 2

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -239,6 +239,8 @@ const DashboardPage = () => {
                 <motion.div
                   initial={{ scale: 0 }} // Start from a smaller scale
                   animate={{ scale: 1 }} // Animate to default scale
+                  whileHover={{ scale: 1.2 }}
+                  whileTap={{ scale: 0.8 }}
                   transition={{
                     type: "spring",
                     stiffness: 100,
